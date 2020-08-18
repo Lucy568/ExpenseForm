db462221680b0da78cb2ec448183d902871b745e
okeke chidinma <chidinma1365@gmail.com>
2020-08-18 16:51:52 +0100

    i added packages repository class

---
+++ src/main/kotlin/com/example/demo/model/ExpenseForm.kt
@@ -0,0 +1,11 @@
+package com.example.demo.model
+
+import java.util.*
+
+class ExpenseForm {
+    var expense_for : String? = ""
+    var amount : String? = ""
+    var created_date : Calendar? = null
+    var updated_at : Calendar? = null
+    var description : String? = ""
+}
